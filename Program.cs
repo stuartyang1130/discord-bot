164dd1e1c2c73de492d64631c1142031d70bab6f
stuartyang1130 <stuartyang1130@gmail.com>
2024-05-09 22:18:43 -0700

    feat: create discord bot

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+class Program
+{
+    private DiscordSocketClient _client;
+
+    static async Task Main(string[] args)
+    {
+        var program = new Program();
+        await program.RunBotAsync();
+    }
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += LogAsync;
+        // _client.Ready += ReadyAsync;
+        _client.MessageReceived += MessageReceivedAsync;
+
+        await _client.LoginAsync(TokenType.Bot, "YOUR_BOT_TOKEN");
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+    private async Task MessageReceivedAsync(SocketMessage message)
+    {
+        if(message.Content.ToLower() == "hello")
+        {
+            await message.Channel.SendMessageAsync("Hello, Discord!");
+        }
+    }
+}
